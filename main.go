3bc05dba0ef0ccfc5b7fe34b5953444b928bf75c
Rachel Sharp <rachel@hashicorp.com>
2021-07-13 10:58:07 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 
-	"github.com/hashicorp/terraform-provider-hashicups/hashicups"
+	"github.com/tr0njavolta/terraform-provider-hashicups/hashicups"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() *schema.Provider {
-			return hashicups.Provider()
-		},
+	tfsdk.Serve(context.Background(), provider.New, tfsdk.ServeOpts{
+		Name: "hashicups",
 	})
 }
